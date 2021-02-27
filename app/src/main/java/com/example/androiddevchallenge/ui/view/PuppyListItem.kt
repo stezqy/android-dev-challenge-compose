3b17ec4172ffa491d893fb4e8846ddc308306aa7
stezqy <1217801722@qq.com>
2021-02-27 23:01:53 +0800

    fix lint

--- app/src/main/java/com/example/androiddevchallenge/ui/view/PuppyListItem.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/view/PuppyListItem.kt
@@ -1,7 +1,11 @@
 package com.example.androiddevchallenge.ui.view
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
